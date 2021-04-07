0f2b286734718a39fa478e74adf7c3106158df33
Paul Mackay <paul.m.mackay85@gmail.com>
2021-04-07 15:53:52 +0100

    Inserting Data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+    $user = User::findOrFail(1);
+
+    // $post = new Post(['title'=>'My first post', 'body'=>'I love Laravel']);
+    // $user->posts()->save($post);
+    // these two lines can be shortend to:
+    $user->posts()->save(new Post(['title'=>'My first post', 'body'=>'I love Laravel']));
 });
